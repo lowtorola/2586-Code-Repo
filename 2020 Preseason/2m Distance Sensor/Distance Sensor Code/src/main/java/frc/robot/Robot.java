7adf8e86dec111aa879a5cd2a14f6ebe5f61c841
Lowell Torola <loWALL2586@gmail.com>
2019-09-30 17:33:38 -0400

    Distance Sensor Debug

--- 2020 Preseason/2m Distance Sensor/Distance Sensor Code/src/main/java/frc/robot/Robot.java
+++ 2020 Preseason/2m Distance Sensor/Distance Sensor Code/src/main/java/frc/robot/Robot.java
@@ -14,59 +14,34 @@ import com.revrobotics.*;
 import com.revrobotics.Rev2mDistanceSensor.Port;
 import com.revrobotics.Rev2mDistanceSensor.RangeProfile;
 
-
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  
+ 
   private Rev2mDistanceSensor distSens;
-
-
-
 
   @Override
   public void robotInit() {
-   
     
-
   }
 
- 
-  
-   
   @Override
   public void robotPeriodic() {
   }
 
-  
   @Override
   public void autonomousInit() {
-   
+
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
+ 
   @Override
   public void autonomousPeriodic() {
-    
+
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
   @Override
   public void teleopPeriodic() {
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
   @Override
   public void testPeriodic() {
   }
