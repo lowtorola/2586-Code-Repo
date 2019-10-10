ca5fd67f3367eef626b69096f7e349c3f5ece560
Lowell Torola <loWALL2586@gmail.com>
2019-10-10 12:28:19 -0400

    Initial Deploy

---
+++ 2020 Preseason/MiniBot Code/MiniBot Drivebase Code/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,84 @@
+
+package frc.robot;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Robot extends TimedRobot {
+
+   DifferentialDrive m_drive;
+
+   Joystick m_joystick = new Joystick(0);
+   double drive_speed;
+   double drive_rotate;
+
+   CANSparkMax f_leftMotor;
+   CANSparkMax r_leftMotor;
+   CANSparkMax f_rightMotor;
+   CANSparkMax r_rightMotor;
+
+  @Override
+  public void robotInit() {
+
+
+    f_leftMotor = new CANSparkMax(3, MotorType.kBrushless);
+    r_leftMotor = new CANSparkMax(5, MotorType.kBrushless);
+    f_rightMotor = new CANSparkMax(6, MotorType.kBrushless);
+    r_rightMotor = new CANSparkMax(2, MotorType.kBrushless);
+
+    f_leftMotor.setSmartCurrentLimit(40);
+    r_leftMotor.setSmartCurrentLimit(40);
+    f_rightMotor.setSmartCurrentLimit(40);
+    r_rightMotor.setSmartCurrentLimit(40);
+
+    SpeedControllerGroup leftMotors = new SpeedControllerGroup(f_leftMotor, r_leftMotor);
+    SpeedControllerGroup rightMotors = new SpeedControllerGroup(f_rightMotor, r_rightMotor);
+
+    m_drive = new DifferentialDrive(leftMotors, rightMotors);
+
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+  
+    drive_speed = -1 * m_joystick.getRawAxis(1);
+    drive_rotate = m_joystick.getRawAxis(2);
+
+    m_drive.arcadeDrive(drive_speed, drive_rotate);
+
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
+}
