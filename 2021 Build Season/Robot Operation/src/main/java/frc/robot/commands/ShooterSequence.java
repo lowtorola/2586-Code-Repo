a53c151229a68b73e84b242a6bd017e52130c5cf
Lowell Torola <loWALL2586@gmail.com>
2021-03-19 18:57:24 -0400

    added shooter logic

---
+++ 2021 Build Season/Robot Operation/src/main/java/frc/robot/commands/ShooterSequence.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShooterSequence extends ParallelCommandGroup {
+    
+    public ShooterSequence(ShooterSubsystem shooterSub, LimelightSubsystem limelight) {
+
+    }
+}
