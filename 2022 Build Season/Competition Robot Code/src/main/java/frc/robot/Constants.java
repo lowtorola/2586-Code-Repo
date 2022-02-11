bfdd360bd39062b2a4da9f553b1705159de10bf1
Lowell Torola <loWALL2586@gmail.com>
2022-02-11 18:22:09 -0500

    Unstable robot drive code

--- 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/Constants.java
+++ 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,106 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    /**
+     * This class contains all drivebase constants & limelight constants, as the limelight is contained within
+     * the drivetrain subsystem for lambda reasons!!
+     */
+    public static final class DriveConstants {
+    /**
+     * The left-to-right distance between the drivetrain wheels
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.572;
+    /**
+     * The front-to-back distance between the drivetrain wheels.
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.572;
+
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 4;
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 3;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 10;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(216.65); 
+
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 1;
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 9;
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(80.42); 
+
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 8;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7;
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 12;
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(48.16); // FIXME Measure and set back left steer offset
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 5;
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11;
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(1.58); // FIXME Measure and set back right steer offset
+    }
+
+    /**
+     * This class contains all operator interface (OI) constants for the driver DS4, operator DS4, and fight
+     * stick. Port numbers are from Driver Station, DS4 and button board constants are subclassed.
+     */
+    public static final class OIConstants {
+        public static final int DRIVER_PORT = 0;
+        public static final int OPERATOR_PORT = 1;
+        public static final int BUTTON_BOARD = 2;
+
+        /**
+         * This class contains button mapping for the Dualshock 4 controllers.
+         */
+        public static final class DS4 {
+            // Buttons / axis clicks
+            public static final int SQUARE = 1;
+            public static final int X = 2;
+            public static final int CIRCLE = 3;
+            public static final int TRIANGLE = 4;
+            public static final int R_BUMPER = 5;
+            public static final int L_BUMPER = 6;
+            public static final int R_TRIGBUTTON = 7;
+            public static final int L_TRIGBUTTON = 8;
+            public static final int SHARE = 9;
+            public static final int OPTIONS = 10;
+            public static final int L_STICK = 11;
+            public static final int R_STICK = 12;
+            public static final int PS_LOGO = 13;
+            public static final int CENTER_PAD = 14;
+            // Axes
+            public static final int L_STICK_X = 0; // left is -1
+            public static final int L_STICK_Y = 1; // forward is -1
+            public static final int R_STICK_X = 2; // left is -1
+            public static final int R_STICK_Y = 5; // forward is -1
+            public static final int L_TRIGGER = 3; // -1 at rest, scales to 1
+            public static final int R_TRIGGER = 4; // -1 at rest, scales to 1
+        }
+        /**
+         * This class contains button bindings for the Fight Stick
+         */
+        public static class FIGHT_STICK {
+            // Buttons
+            public static final int A = 1;
+            public static final int B = 2;
+            public static final int X = 3;
+            public static final int Y = 4;
+            public static final int L_BUMPER = 5;
+            public static final int R_BUMPER = 6;
+            public static final int SHARE = 7;
+            public static final int OPTIONS = 8;
+            public static final int L3 = 9; // DO NOT USE!!!
+            public static final int R3 = 10; // DO NOT USE!!!
+            // Axes
+            public static final int X_AXIS = 0; // Left = -1, Center = 0, Right = 1
+            public static final int Y_AXIS = 1; // Forward = -1, Center = 0, Back = 1
+            public static final int L_TRIGGER = 2; // Rest = 0, Pressed = 1
+            public static final int R_TRIGGER = 3; // Rest = 0, Pressed = 1
+            
+        }
+
+    }
+
+}
--- 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/RobotContainer.java
+++ 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,16 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.DefaultDriveCommand;
+
+import static frc.robot.Constants.OIConstants.*;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,13 +23,31 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+
+  private final Joystick m_driver = new Joystick(DRIVER_PORT);
+  // private final Joystick m_operator = new Joystick(OPERATOR_PORT);
+
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    // Set up the default command for the drivetrain.
+    // The controls are for field-oriented driving:
+    // Left stick Y axis -> forward and backwards movement
+    // Left stick X axis -> left and right movement
+    // Right stick X axis -> rotation
+    m_driveSubsystem.setDefaultCommand(new DefaultDriveCommand(
+            m_driveSubsystem,
+            () -> -modifyAxis(m_driver.getRawAxis(DS4.L_STICK_Y)) * DriveSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+            () -> -modifyAxis(m_driver.getRawAxis(DS4.L_STICK_X)) * DriveSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+            () -> -modifyAxis(m_driver.getRawAxis(DS4.R_STICK_X)) * DriveSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+    ));
+
+
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -34,7 +58,15 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+
+    // Share button zeros the gyro
+    new JoystickButton(m_driver, DS4.SHARE)
+    // no requirements since we don't have to interrupt anything
+    .whenPressed(m_driveSubsystem::zeroGyroscope);
+    
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -45,4 +77,27 @@ public class RobotContainer {
     // An ExampleCommand will run in autonomous
     return m_autoCommand;
   }
+
+  private static double deadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
+  }
+
+  private static double modifyAxis(double value) {
+    // Deadband
+    value = deadband(value, 0.05);
+
+    // Square the axis
+    value = Math.copySign(value * value, value);
+
+    return value;
+  }
+
 }
