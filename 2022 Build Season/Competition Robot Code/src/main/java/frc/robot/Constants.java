12739f6dea66e26008c83be26121f2535a0dcb54
Lowell Torola <loWALL2586@gmail.com>
2022-03-06 19:17:15 -0500

    added possibly-functional PID implementation uses onboard TalonFX controllers to control drive and steer PID

--- 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/Constants.java
+++ 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/Constants.java
@@ -41,25 +41,65 @@ public final class Constants {
      */
     public static final double DRIVETRAIN_WHEELBASE_METERS = 0.572;
 
+    // Front left module
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 4;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 3;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 10;
     public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(216.65); 
+    // PID constants for Front Left Module
+    public static final double FRONT_LEFT_MODULE_DRIVE_KP = 0.1; // FIXME: get from sysid values
+    public static final double FRONT_LEFT_MODULE_DRIVE_KI = 0.0; // FIXME: get from sysid values
+    public static final double FRONT_LEFT_MODULE_DRIVE_KD = 0.01; // FIXME: get from sysid values
+    public static final double FRONT_LEFT_MODULE_DRIVE_KF = 0.0; // FIXME: get from sysid values
+    public static final double FRONT_LEFT_MODULE_TURN_KP = 0.1; // FIXME: get from sysid values
+    public static final double FRONT_LEFT_MODULE_TURN_KI = 0.0; // FIXME: get from sysid values
+    public static final double FRONT_LEFT_MODULE_TURN_KD = 0.01; // FIXME: get from sysid values
+    public static final double FRONT_LEFT_MODULE_TURN_KF = 0.0; // FIXME: get from sysid values
 
+    // front right module
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 21; // FIXME: Change this to fit into range
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 21;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 9;
     public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(80.42); 
+    // PID constants for Front Right Module
+    public static final double FRONT_RIGHT_MODULE_DRIVE_KP = 0.1; // FIXME: get from sysid values
+    public static final double FRONT_RIGHT_MODULE_DRIVE_KI = 0.0; // FIXME: get from sysid values
+    public static final double FRONT_RIGHT_MODULE_DRIVE_KD = 0.01; // FIXME: get from sysid values
+    public static final double FRONT_RIGHT_MODULE_DRIVE_KF = 0.0; // FIXME: get from sysid values
+    public static final double FRONT_RIGHT_MODULE_TURN_KP = 0.1; // FIXME: get from sysid values
+    public static final double FRONT_RIGHT_MODULE_TURN_KI = 0.0; // FIXME: get from sysid values
+    public static final double FRONT_RIGHT_MODULE_TURN_KD = 0.01; // FIXME: get from sysid values
+    public static final double FRONT_RIGHT_MODULE_TURN_KF = 0.0; // FIXME: get from sysid values
 
+    // back left module
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 8;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 12;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(137.81); // FIXME Measure and set back left steer offset
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(137.81); 
+    // PID constants for Back Left Module
+    public static final double BACK_LEFT_MODULE_DRIVE_KP = 0.1; // FIXME: get from sysid values
+    public static final double BACK_LEFT_MODULE_DRIVE_KI = 0.0; // FIXME: get from sysid values
+    public static final double BACK_LEFT_MODULE_DRIVE_KD = 0.01; // FIXME: get from sysid values
+    public static final double BACK_LEFT_MODULE_DRIVE_KF = 0.0; // FIXME: get from sysid values
+    public static final double BACK_LEFT_MODULE_TURN_KP = 0.1; // FIXME: get from sysid values
+    public static final double BACK_LEFT_MODULE_TURN_KI = 0.0; // FIXME: get from sysid values
+    public static final double BACK_LEFT_MODULE_TURN_KD = 0.01; // FIXME: get from sysid values
+    public static final double BACK_LEFT_MODULE_TURN_KF = 0.0; // FIXME: get from sysid values
 
+    // back right module
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 5;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(272.55); // FIXME Measure and set back right steer offset
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(272.55);
+    // PID constants for Back Right Module
+    public static final double BACK_RIGHT_MODULE_DRIVE_KP = 0.1; // FIXME: get from sysid values
+    public static final double BACK_RIGHT_MODULE_DRIVE_KI = 0.0; // FIXME: get from sysid values
+    public static final double BACK_RIGHT_MODULE_DRIVE_KD = 0.01; // FIXME: get from sysid values
+    public static final double BACK_RIGHT_MODULE_DRIVE_KF = 0.0; // FIXME: get from sysid values
+    public static final double BACK_RIGHT_MODULE_TURN_KP = 0.1; // FIXME: get from sysid values
+    public static final double BACK_RIGHT_MODULE_TURN_KI = 0.0; // FIXME: get from sysid values
+    public static final double BACK_RIGHT_MODULE_TURN_KD = 0.01; // FIXME: get from sysid values
+    public static final double BACK_RIGHT_MODULE_TURN_KF = 0.0; // FIXME: get from sysid values
     }
 
     /**
--- 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,6 +4,13 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.RemoteFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.PigeonIMU;
 import com.kauailabs.navx.frc.AHRS;
 import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
@@ -24,6 +31,7 @@ import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -86,6 +94,21 @@ public class DriveSubsystem extends SubsystemBase {
   private final SwerveModule m_backLeftModule;
   private final SwerveModule m_backRightModule;
 
+  // Swerve motors, for PID control
+  private final WPI_TalonFX m_frontLeftDrive;
+  private final WPI_TalonFX m_frontLeftTurn;
+  private final CANCoder m_frontLeftTurnEncoder;
+  private final CANCoder m_frontRightTurnEncoder;
+  private final CANCoder m_backLeftTurnEncoder;
+  private final CANCoder m_backRightTurnEncoder;
+  private final WPI_TalonFX m_frontRightDrive;
+  private final WPI_TalonFX m_frontRightTurn;
+  private final WPI_TalonFX m_backLeftDrive;
+  private final WPI_TalonFX m_backLeftTurn;
+  private final WPI_TalonFX m_backRightDrive;
+  private final WPI_TalonFX m_backRightTurn;
+
+
   private ChassisSpeeds m_chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
 
   public final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics, getGyroscopeRotation());
@@ -93,23 +116,6 @@ public class DriveSubsystem extends SubsystemBase {
   public DriveSubsystem() {
 
     ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
-
-    // There are 4 methods you can call to create your swerve modules.
-    // The method you use depends on what motors you are using.
-    //
-    // Mk3SwerveModuleHelper.createFalcon500(...)
-    //   Your module has two Falcon 500s on it. One for steering and one for driving.
-    //
-    // Mk3SwerveModuleHelper.createNeo(...)
-    //   Your module has two NEOs on it. One for steering and one for driving.
-    //
-    // Mk3SwerveModuleHelper.createFalcon500Neo(...)
-    //   Your module has a Falcon 500 and a NEO on it. The Falcon 500 is for driving and the NEO is for steering.
-    //
-    // Mk3SwerveModuleHelper.createNeoFalcon500(...)
-    //   Your module has a NEO and a Falcon 500 on it. The NEO is for driving and the Falcon 500 is for steering.
-    //
-    // Similar helpers also exist for Mk4 modules using the Mk4SwerveModuleHelper class.
 
     // By default we will use Falcon 500s in standard configuration. But if you use a different configuration or motors
     // you MUST change it. If you do not, your code will crash on startup.
@@ -129,8 +135,20 @@ public class DriveSubsystem extends SubsystemBase {
             // This is how much the steer encoder is offset from true zero (In our case, zero is facing straight forward)
             FRONT_LEFT_MODULE_STEER_OFFSET
     );
-
-    
+    // Access drive & turn motors, then set PIDF constants for closed-loop control
+    // drive motor
+    m_frontLeftDrive = (WPI_TalonFX) m_frontLeftModule.getDriveMotor();
+    m_frontLeftDrive.config_kP(0, FRONT_LEFT_MODULE_DRIVE_KP);
+    m_frontLeftDrive.config_kI(0, FRONT_LEFT_MODULE_DRIVE_KI);
+    m_frontLeftDrive.config_kD(0, FRONT_LEFT_MODULE_DRIVE_KD);
+    m_frontLeftDrive.config_kF(0, FRONT_LEFT_MODULE_DRIVE_KF);
+    // turn motor
+    m_frontLeftTurn = (WPI_TalonFX) m_frontLeftModule.getSteerMotor();
+    m_frontLeftTurnEncoder = (CANCoder) m_frontLeftModule.getSteerEncoder();
+    m_frontLeftTurn.config_kP(0, FRONT_LEFT_MODULE_TURN_KP);
+    m_frontLeftTurn.config_kI(0, FRONT_LEFT_MODULE_TURN_KI);
+    m_frontLeftTurn.config_kD(0, FRONT_LEFT_MODULE_TURN_KD);
+    m_frontLeftTurn.config_kF(0, FRONT_LEFT_MODULE_TURN_KF);
 
     // We will do the same for the other modules
     m_frontRightModule = Mk4SwerveModuleHelper.createFalcon500(
@@ -143,6 +161,20 @@ public class DriveSubsystem extends SubsystemBase {
             FRONT_RIGHT_MODULE_STEER_ENCODER,
             FRONT_RIGHT_MODULE_STEER_OFFSET
     );
+    // Access drive & turn motors, then set PIDF constants for closed-loop control
+    // drive motor
+    m_frontRightDrive = (WPI_TalonFX) m_frontRightModule.getDriveMotor();
+    m_frontRightDrive.config_kP(0, FRONT_RIGHT_MODULE_DRIVE_KP);
+    m_frontRightDrive.config_kI(0, FRONT_RIGHT_MODULE_DRIVE_KI);
+    m_frontRightDrive.config_kD(0, FRONT_RIGHT_MODULE_DRIVE_KD);
+    m_frontRightDrive.config_kF(0, FRONT_RIGHT_MODULE_DRIVE_KF);
+    // turn motor
+    m_frontRightTurn = (WPI_TalonFX) m_frontRightModule.getSteerMotor();
+    m_frontRightTurnEncoder = (CANCoder) m_frontRightModule.getSteerEncoder();
+    m_frontRightTurn.config_kP(0, FRONT_RIGHT_MODULE_TURN_KP);
+    m_frontRightTurn.config_kI(0, FRONT_RIGHT_MODULE_TURN_KI);
+    m_frontRightTurn.config_kD(0, FRONT_RIGHT_MODULE_TURN_KD);
+    m_frontRightTurn.config_kF(0, FRONT_RIGHT_MODULE_TURN_KF);
 
     m_backLeftModule = Mk4SwerveModuleHelper.createFalcon500(
             tab.getLayout("Back Left Module", BuiltInLayouts.kList)
@@ -154,6 +186,21 @@ public class DriveSubsystem extends SubsystemBase {
             BACK_LEFT_MODULE_STEER_ENCODER,
             BACK_LEFT_MODULE_STEER_OFFSET
     );
+    // Access drive & turn motors, then set PIDF constants for closed-loop control
+    // drive motor
+    m_backLeftDrive = (WPI_TalonFX) m_backLeftModule.getDriveMotor();
+    m_backLeftDrive.config_kP(0, BACK_LEFT_MODULE_DRIVE_KP);
+    m_backLeftDrive.config_kI(0, BACK_LEFT_MODULE_DRIVE_KI);
+    m_backLeftDrive.config_kD(0, BACK_LEFT_MODULE_DRIVE_KD);
+    m_backLeftDrive.config_kF(0, BACK_LEFT_MODULE_DRIVE_KF);
+    // turn motor
+    m_backLeftTurn = (WPI_TalonFX) m_backLeftModule.getSteerMotor();
+    m_backLeftTurnEncoder = (CANCoder) m_frontRightModule.getSteerEncoder();
+    m_backLeftTurn.config_kP(0, BACK_LEFT_MODULE_TURN_KP);
+    m_backLeftTurn.config_kI(0, BACK_LEFT_MODULE_TURN_KI);
+    m_backLeftTurn.config_kD(0, BACK_LEFT_MODULE_TURN_KD);
+    m_backLeftTurn.config_kF(0, BACK_LEFT_MODULE_TURN_KF);
+    
 
     m_backRightModule = Mk4SwerveModuleHelper.createFalcon500(
             tab.getLayout("Back Right Module", BuiltInLayouts.kList)
@@ -165,7 +212,20 @@ public class DriveSubsystem extends SubsystemBase {
             BACK_RIGHT_MODULE_STEER_ENCODER,
             BACK_RIGHT_MODULE_STEER_OFFSET
     );
-
+    // Access drive & turn motors, then set PIDF constants for closed-loop control
+    // drive motor
+    m_backRightDrive = (WPI_TalonFX) m_backRightModule.getDriveMotor();
+    m_backRightDrive.config_kP(0, BACK_RIGHT_MODULE_DRIVE_KP);
+    m_backRightDrive.config_kI(0, BACK_RIGHT_MODULE_DRIVE_KI);
+    m_backRightDrive.config_kD(0, BACK_RIGHT_MODULE_DRIVE_KD);
+    m_backRightDrive.config_kF(0, BACK_RIGHT_MODULE_DRIVE_KF);
+    // turn motor
+    m_backRightTurn = (WPI_TalonFX) m_backRightModule.getSteerMotor();
+    m_backRightTurnEncoder = (CANCoder) m_backRightModule.getSteerEncoder();
+    m_backRightTurn.config_kP(0, BACK_RIGHT_MODULE_TURN_KP);
+    m_backRightTurn.config_kI(0, BACK_RIGHT_MODULE_TURN_KI);
+    m_backRightTurn.config_kD(0, BACK_RIGHT_MODULE_TURN_KD);
+    m_backRightTurn.config_kF(0, BACK_RIGHT_MODULE_TURN_KF);
   }
 
   /**
@@ -185,6 +245,21 @@ public class DriveSubsystem extends SubsystemBase {
 
   public void drive(ChassisSpeeds chassisSpeeds) {
     m_chassisSpeeds = chassisSpeeds;
+  }
+
+  public void drivePID(ChassisSpeeds chassisSpeeds) {
+        SwerveModuleState[] states = m_kinematics.toSwerveModuleStates(chassisSpeeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
+        m_frontLeftDrive.set(TalonFXControlMode.Velocity, toClosedLoopUnits(states[0].speedMetersPerSecond));
+        m_frontRightDrive.set(TalonFXControlMode.Velocity, toClosedLoopUnits(states[1].speedMetersPerSecond));
+        m_backLeftDrive.set(TalonFXControlMode.Velocity, toClosedLoopUnits(states[2].speedMetersPerSecond));
+        m_backRightDrive.set(TalonFXControlMode.Velocity, toClosedLoopUnits(states[3].speedMetersPerSecond));
+        m_frontLeftTurn.set(TalonFXControlMode.Position, states[0].angle.getRadians());
+      }
+
+  public double toClosedLoopUnits(double metersPerSecond){
+        double RPM = metersPerSecond * 60.0 * (1./0.3191) * 6.75;
+        return RPM * 2048.0 / 600.0;
   }
 
   @Override
