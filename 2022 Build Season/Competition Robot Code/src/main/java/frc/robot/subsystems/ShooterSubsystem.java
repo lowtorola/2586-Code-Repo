734b8791108d953511b3ff179a778b7b00d97328
Lowell Torola <loWALL2586@gmail.com>
2022-03-07 23:17:19 -0500

    ditto

--- 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2022 Build Season/Competition Robot Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -12,6 +12,8 @@ import static frc.robot.Constants.ShooterConstants.*;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class ShooterSubsystem extends SubsystemBase {
@@ -21,13 +23,21 @@ public class ShooterSubsystem extends SubsystemBase {
     private final DigitalInput m_topBB = new DigitalInput(TOP_BB);
     private final DigitalInput m_bottomBB = new DigitalInput(BOTTOM_BB);
     private RelativeEncoder m_encoder;
-
+    private SparkMaxPIDController m_pidController;
     int feederState;
 
   /** Creates a new ExampleSubsystem. */
   public ShooterSubsystem() {
     m_encoder = m_flywheel.getEncoder();
     m_feeder.setInverted(true);
+
+    m_pidController = m_flywheel.getPIDController();
+    m_pidController.setP(KP);
+    m_pidController.setI(KI);
+    m_pidController.setD(KD);
+    m_pidController.setIZone(KIZ);
+    m_pidController.setFF(KFF);
+    m_pidController.setOutputRange(KMIN_OUTPUT, KMAX_OUTPUT);
   }
 
   /**
@@ -35,6 +45,13 @@ public class ShooterSubsystem extends SubsystemBase {
    */
   public void shootVolts() {
       m_flywheel.setVoltage(SHOOT_VOLTS);
+  }
+
+  /**
+   * Sets the shooter to a given speed (RPM)
+   */
+  public void shootRPM(double speed) {
+    m_pidController.setReference(speed, ControlType.kVelocity);
   }
 
   /**
